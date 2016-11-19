5892c521cced2360c2179a3e284242baea0245c0
Will Graham <grahamw0@students.rowan.edu>
2016-11-19 12:02:27 -0500

    Tested Selection Sort

--- src/arrayList/Driver.java
+++ src/arrayList/Driver.java
@@ -15,12 +15,14 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-    Random rand = new Random();
+    Random rand = new Random(666);
     ListArray list = new ListArray();
     for(int i = 0; i < 20; i++) {
       list.add(rand.nextInt(100));
     }
-    list.bubbleSort();
+    list.add(0);
+    list.add(0);
+    list.selectionSort();
     
     for(Comparable c : list.toArray()) {
       System.out.print(c + " ");
